293195016fc972baff61a015d533b19c9d45f1de
atheeth panchakshare <atheeth.iccc@gmail.com>
2024-08-10 12:45:48 +0530

    My First Nextjs 14 Language app starter tamplet

---
+++ src/app/[lang]/dictionaries.ts
@@ -0,0 +1,12 @@
+import 'server-only'
+
+export type Locale = keyof typeof dictionaries
+
+const dictionaries = {
+  en: () => import('./dictionaries/en.json').then((module) => module.default),
+  de: () => import('./dictionaries/de.json').then((module) => module.default),
+  cn: () => import('./dictionaries/cn.json').then((module) => module.default),
+  hi: () => import('./dictionaries/hi.json').then((module) => module.default),
+}
+
+export const getDictionary = async (locale: Locale) => dictionaries[locale]()
